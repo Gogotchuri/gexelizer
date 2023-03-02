4a8f8710b7dcb5d09a3c5bc378526c68e58816b4
Gogotchuri <igogo17@freeuni.edu.ge>
2023-03-02 18:08:17 +0400

    initial commit 1

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import "github.com/go-the-way/exl"
+
+type Item struct {
+	Name  string  `excel:"name"`
+	Price float64 `excel:"price"`
+}
+
+type Document struct {
+	ID          uint    `excel:"id"`
+	TotalAmount float64 `excel:"total_amount"`
+	Items       []Item  `excel:"items"`
+}
+
+func (d Document) Configure(c *exl.WriteConfig) {
+
+}
+
+func main() {
+	if err := exl.Write("test.xlsx", []*Document{
+		{
+			ID:          1,
+			TotalAmount: 100,
+			Items: []Item{
+				{
+					Name: "item1",
+				},
+				{
+					Name: "item2",
+				},
+			},
+		},
+		{
+			ID:          2,
+			TotalAmount: 200,
+			Items: []Item{
+				{
+					Name: "item3",
+				},
+			},
+		},
+	}); err != nil {
+		panic(err)
+	}
+}
